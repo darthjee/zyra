3352fc548420b67a9b88bc05ba67bba63682f6a4
darthjee <darthjee@gmail.com>
2022-08-31 17:50:21 -0300

    Add documentation

--- lib/zyra/builder.rb
+++ lib/zyra/builder.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 
 module Zyra
+  # @api private
+  # @author Darthjee
+  #
+  # Class responsible for building a model
   class Builder
     attr_reader :model_class
 
+    # @param model_class [Class] Model class to be initialized
+    #   into a model
     def initialize(model_class)
       @model_class = model_class
     end
 
+    # Builds an instance of model_class
+    #
+    # @param attributes [Hash] attributes to be set in the model
+    # @param block [Proc] block to be ran after where more attributes
+    # will be set
+    #
+    # @yield [Setter] Setter wrapping model
     def build(**attributes, &block)
       model_class.new(attributes).tap do |model|
         Setter.new(model).tap(&block) if block
