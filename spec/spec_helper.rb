9866ae913e6135b05ec60ecbf5a34a06ccbf9417
darthjee <darthjee@gmail.com>
2022-08-31 10:31:12 -0300

    Use active record

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ SimpleCov.start 'gem'
 require 'zyra'
 require 'pry-nav'
 
+require 'active_record'
+
 support_files = File.expand_path('spec/support/**/*.rb')
 
 Dir[support_files].sort.each { |file| require file }
