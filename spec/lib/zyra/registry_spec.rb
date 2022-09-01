0707c9255a5ccb56be5028f2341e51802050f0ba
darthjee <darthjee@gmail.com>
2022-09-01 06:54:36 -0300

    Improove specs

--- spec/lib/zyra/registry_spec.rb
+++ spec/lib/zyra/registry_spec.rb
@@ -6,23 +6,49 @@ describe Zyra::Registry do
   let(:registry) { described_class.new }
 
   describe '#register' do
-    it do
-      expect(registry.register(User, as: :user))
-        .to be_a(Zyra::Builder)
+    context 'when providing symbol alias' do
+      let(:key) { :user_alias }
+
+      it do
+        expect(registry.register(User, as: key))
+          .to be_a(Zyra::Builder)
+      end
+
+      it 'creates a builder for the given class' do
+        expect(registry.register(User, as: key))
+          .to eq(Zyra::Builder.new(User))
+      end
+
+      it 'register builder under the key' do
+        expect { registry.register(User, as: key) }
+          .to change { registry.builder_for(key) }
+          .from(nil).to(Zyra::Builder.new(User))
+      end
     end
 
-    it 'creates a builder for the given class' do
-      expect(registry.register(User, as: :user))
-        .to eq(Zyra::Builder.new(User))
-    end
+    context 'when providing string alias' do
+      let(:key) { 'user' }
 
-    it 'register builder under the key' do
-      expect { registry.register(User, as: :user) }
-        .to change { registry.builder_for(:user) }
-        .from(nil).to(Zyra::Builder.new(User))
+      it do
+        expect(registry.register(User, as: key))
+          .to be_a(Zyra::Builder)
+      end
+
+      it 'creates a builder for the given class' do
+        expect(registry.register(User, as: key))
+          .to eq(Zyra::Builder.new(User))
+      end
+
+      it 'register builder under the key' do
+        expect { registry.register(User, as: key) }
+          .to change { registry.builder_for(key) }
+          .from(nil).to(Zyra::Builder.new(User))
+      end
     end
 
     context 'when not providing an alias' do
+      let(:key) { :user }
+
       it do
         expect(registry.register(User))
           .to be_a(Zyra::Builder)
@@ -35,7 +61,7 @@ describe Zyra::Registry do
 
       it 'register builder under the correct key' do
         expect { registry.register(User) }
-          .to change { registry.builder_for(:user) }
+          .to change { registry.builder_for(key) }
           .from(nil).to(Zyra::Builder.new(User))
       end
     end
