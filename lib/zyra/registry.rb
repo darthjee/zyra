ad7c443c3b2d3e5ffa95800a664e3c4c2c0366b8
darthjee <darthjee@gmail.com>
2022-09-01 06:46:00 -0300

    Add missing alias

--- lib/zyra/registry.rb
+++ lib/zyra/registry.rb
@@ -2,7 +2,7 @@
 
 module Zyra
   class Registry
-    def register(klass, as:)
+    def register(klass, as: klass.name.gsub(/::([A-Z])/, '_\1').downcase)
       registry[as] = Builder.new(klass)
     end
 
--- spec/lib/zyra/registry_spec.rb
+++ spec/lib/zyra/registry_spec.rb
@@ -15,5 +15,17 @@ describe Zyra::Registry do
       expect(registry.register(User, as: :user))
         .to eq(Zyra::Builder.new(User))
     end
+
+    context 'when not providing an alias' do
+      it do
+        expect(registry.register(User))
+          .to be_a(Zyra::Builder)
+      end
+
+      it 'creates a builder for the given class' do
+        expect(registry.register(User))
+          .to eq(Zyra::Builder.new(User))
+      end
+    end
   end
 end
