22d2ff6399a0f1c7c94e1e92ebe95c44c78faaa7
darthjee <darthjee@gmail.com>
2022-09-02 07:54:27 -0300

    Fix factory

--- spec/support/factories/user.rb
+++ spec/support/factories/user.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :user, class: '::User' do
     sequence(:name)  { |n| "Name-#{n}" }
     sequence(:email) { |n| "user-#{n}@email.com" }
-    sequence(:password) { |n| SecureRandom.hex(10) }
+    password         { SecureRandom.hex(10) }
   end
 end
