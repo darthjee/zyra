ff3bfbfae637d9a4b40dd87e57683e883ec4b3bc
darthjee <darthjee@gmail.com>
2022-09-04 14:58:27 -0300

    Add find_by

--- lib/zyra/registry.rb
+++ lib/zyra/registry.rb
@@ -6,7 +6,9 @@ module Zyra
   # Registry of all registered builders
   class Registry
     # (see Zyra.register)
-    def register(klass, key = klass.name.gsub(/::([A-Z])/, '_\1').downcase)
+    def register(klass, key = nil, find_by: [])
+      key ||= klass.name.gsub(/::([A-Z])/, '_\1').downcase
+
       registry[key.to_sym] = FinderCreator.new(klass, [])
     end
 
