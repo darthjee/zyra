ff6e98032c21bd55da3084cb21a0ae9401d7b81f
darthjee <darthjee@gmail.com>
2022-09-04 11:07:42 -0300

    Improve doc

--- lib/zyra/finder.rb
+++ lib/zyra/finder.rb
@@ -16,6 +16,7 @@ module Zyra
       @event_registry = event_registry
     end
 
+    # @api public
     # Search the entry in the database
     #
     # The query is done using part of the expected
--- lib/zyra/finder_creator.rb
+++ lib/zyra/finder_creator.rb
@@ -29,6 +29,53 @@ module Zyra
       tap { event_registry.register(event, &block) }
     end
 
+    # @method find
+    # @api public
+    #
+    # Search the entry in the database
+    #
+    # The query is done using part of the expected
+    # attributes filtered by the configured keys}
+    #
+    # if the model is found an event is triggered
+    #
+    # @overload find(attributes)
+    #   @param attributes [Hash] expected model attribiutes
+    #
+    # @return [Object] the model from the database
+    delegate :find, to: :finder
+
+    # @method build
+    # @api public
+    #
+    # Builds an instance of the registered model class
+    #
+    # @overload build(attributes, &block)
+    #   @param attributes [Hash] attributes to be set in the model
+    #   @param block [Proc] block to be ran after where more attributes
+    #   will be set
+    #
+    # @yield [Object] Instance of the model class
+    #
+    # @return [Object] an instance of model class
+
+    # @method create
+    # @api public
+    #
+    # Creates an instance of the registered model class
+    #
+    # This behaves like {#build}, but persists the entry
+    #
+    # @overload create(attributes, &block)
+    #   @param attributes [Hash] attributes to be set in the model
+    #   @param block [Proc] block to be ran after where more attributes
+    #   will be set
+    #
+    # @yield [Object] Instance of the model class
+    #
+    # @return [Object] an instance of model class
+    delegate :build, :create, to: :builder
+
     protected
 
     # @method model_class
@@ -46,9 +93,6 @@ module Zyra
     # @return [Array<Symbol>]
     attr_reader :model_class, :keys
 
-    delegate :find, to: :finder
-    delegate :create, to: :builder
-
     # @private
     #
     # Event registry
@@ -61,10 +105,26 @@ module Zyra
       @event_registry ||= Jace::Registry.new
     end
 
+    # @private
+    #
+    # Returns an instance of {Finder}
+    #
+    # Finder will use the same event registry so that event
+    # handling and registration is centralized
+    #
+    # @return Finder
     def finder
       @finder ||= Finder.new(model_class, keys, event_registry: event_registry)
     end
 
+    # @private
+    #
+    # Returns an instance of {Builder}
+    #
+    # Builder will use the same event registry so that event
+    # handling and registration is centralized
+    #
+    # @return Builder
     def builder
       @builder ||= Builder.new(model_class, event_registry: event_registry)
     end
