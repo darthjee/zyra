f47cc7155dd66cc99c9c3104cf7d7947c0933953
darthjee <darthjee@gmail.com>
2022-09-05 12:46:23 -0300

    Add exception documentation

--- lib/zyra/exceptions.rb
+++ lib/zyra/exceptions.rb
@@ -5,6 +5,8 @@ require 'jace'
 
 module Zyra
   module Exceptions
+    # Exception returned when a model has not been registered
+    # and there is an attempt to use it
     class NotRegistered < StandardError; end
   end
 end
