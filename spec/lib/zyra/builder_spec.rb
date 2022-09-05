1a7ab6b91928235823eac3ac0c756bc92d817c82
darthjee <darthjee@gmail.com>
2022-09-05 10:56:54 -0300

    Remove uneeded spec

--- spec/lib/zyra/builder_spec.rb
+++ spec/lib/zyra/builder_spec.rb
@@ -10,50 +10,6 @@ describe Zyra::Builder do
   let(:event_registry) { Jace::Registry.new }
   let(:model_class)    { User }
 
-  describe '#build' do
-    it do
-      expect(builder.build).to be_a(model_class)
-    end
-
-    it do
-      expect(builder.build)
-        .not_to be_persisted
-    end
-
-    context 'when attributes are given' do
-      let(:name) { SecureRandom.hex(10) }
-
-      it 'initializes the model with the given attribute' do
-        expect(builder.build(name: name).name)
-          .to eq(name)
-      end
-    end
-
-    context 'when a block is given' do
-      let(:name) { SecureRandom.hex(10) }
-
-      it 'initializes the model with the given attribute' do
-        value = name
-        expect(builder.build { |model| model.name = value }.name)
-          .to eq(name)
-      end
-    end
-
-    context 'when building has an after build handler' do
-      let(:name) { SecureRandom.hex(10) }
-
-      before do
-        value = name
-        event_registry.register(:build) { |model| model.name = value }
-      end
-
-      it 'runs the event handler' do
-        expect(builder.build.name)
-          .to eq(name)
-      end
-    end
-  end
-
   describe '#create' do
     it do
       expect(builder.create).to be_a(model_class)
