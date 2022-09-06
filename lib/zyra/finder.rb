ccb0e89c47c68ca011e80504af747cffabfcac5f
darthjee <darthjee@gmail.com>
2022-09-06 11:14:44 -0300

    Update docs

--- lib/zyra/finder.rb
+++ lib/zyra/finder.rb
@@ -24,6 +24,10 @@ module Zyra
     # if the model is found an event is triggered
     #
     # @param attributes [Hash] expected model attribiutes
+    # @param block [Proc] block to be ran after where
+    # more attributes will be set
+    #
+    # @yield [Object] Instance of the model class found
     #
     # @return [Object] the model from the database
     def find(attributes, &block)
